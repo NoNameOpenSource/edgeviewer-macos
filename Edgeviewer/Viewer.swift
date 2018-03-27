2f583233517447c8883d0a21f94b847d560e6b96
bmmkac <yzc971210@gmail.com>
2018-03-27 13:14:34 -0400

    add swift file for each viewer controller

---
+++ Edgeviewer/Viewer.swift
@@ -0,0 +1,18 @@
+//
+//  Viewer.swift
+//  Edgeviewer
+//
+//  Created by bmmkac on 3/27/18.
+//  Copyright © 2018 bmmkac. All rights reserved.
+//
+
+import Cocoa
+
+class Viewer: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

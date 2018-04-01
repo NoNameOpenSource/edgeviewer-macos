fae60c3c6ade0c93c328e95d333ba2b316efa84f
bmmkac <yzc971210@gmail.com>
2018-04-01 00:14:52 -0400

    Content View Test pass

---
+++ EdgeViewer/Chapter.swift
@@ -0,0 +1,18 @@
+//
+//  Chapter.swift
+//  Edgeviewer
+//
+//  Created by bmmkac on 4/1/18.
+//  Copyright © 2018 bmmkac. All rights reserved.
+//
+
+import Cocoa
+
+class chapter: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

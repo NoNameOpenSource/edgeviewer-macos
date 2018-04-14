feea6eacd2dfba14730af4dbb85c6a86d832700f
YunSang Lee <qlqhwb@gmail.com>
2018-04-14 04:02:47 -0400

    Added LibraryViewController ( inheriting NSSplitViewController )

---
+++ EdgeViewer/LibraryView/LibraryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryViewController.swift
+//  EdgeViewer
+//
+//  Created by YunSang Lee on 2018. 4. 14..
+//  Copyright © 2018년 NoName. All rights reserved.
+//
+
+import Cocoa
+
+class LibraryViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

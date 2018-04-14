4e2acb55a0863d1a24a6acbae6b5ed3f4db2f783
YunSang Lee <qlqhwb@gmail.com>
2018-04-14 03:28:09 -0400

    Add LibraryListViewController, and connected with sourcelistview controller from storyboard

---
+++ EdgeViewer/LibraryListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryListViewController.swift
+//  EdgeViewer
+//
+//  Created by YunSang Lee on 2018. 4. 14..
+//  Copyright © 2018년 NoName. All rights reserved.
+//
+
+import Cocoa
+
+class LibraryListViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

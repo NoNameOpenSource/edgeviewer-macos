42da66329bc571a6d8545c7e498a8a55e911b8c8
YunSang Lee <qlqhwb@gmail.com>
2018-04-13 23:41:09 -0400

    Add dummy model Delete WindowController

---
+++ EdgeViewer/Manga.swift
@@ -0,0 +1,70 @@
+//
+//  Manga.swift
+//  EdgeViewer
+//
+//  Created by bmmkac on 4/3/18.
+//  Copyright © 2018 NoName. All rights reserved.
+//
+
+import Cocoa
+
+class Manga {
+    var Chapter : [[NSImage]] = [[NSImage()]]
+    //    var currentChapter : Int() {
+    //  get
+    //
+    //}
+    //set {
+    //fuck you
+    
+    //}
+    //}
+    var Author : String = ""
+    var Page : [NSImage] = []
+    var currentPage : Int = 1
+    var PageNumber : Int = 0
+    var CoverPage : NSImage = NSImage()
+    var title : String = ""
+    var rate : Int = 0
+    var chapter : [Int] = [1]
+    var emptyPage : NSImage = NSImage(named: NSImage.Name(rawValue: "blank"))!
+    let cover : NSImage
+    
+    
+    init(Title : String) {
+        self.title = Title
+        self.cover = NSImage()
+        self.cover.backgroundColor = NSColor(calibratedRed: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+    }
+    
+    func addNewPage(Pages : NSImage){
+        self.Page.append(Pages)
+        self.PageNumber += 1
+    }
+    
+    func grabPage () -> NSImage? {
+        return self.Page[self.currentPage - 1] as NSImage
+    }
+    
+    func grabRightPage() -> NSImage? {
+        if self.currentPage == self.PageNumber{
+            return self.emptyPage as NSImage
+        }
+        else {
+            return self.Page[self.currentPage] as NSImage
+        }
+    }
+    
+    func IfNewChapter() -> Bool {
+        var BeginOfChapter = false
+        
+        for Page in chapter {
+            if currentPage == Page {
+                BeginOfChapter = true
+                break
+            }
+        }
+        return BeginOfChapter
+    }
+}
+

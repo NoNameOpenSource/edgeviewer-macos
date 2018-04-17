ff9c037ee7f47833ae4c475da2ff02561787b974
Sanzhar Almakyn <almakyns@gmail.com>
2018-04-16 23:41:40 -0400

    Added the tick on the option Selectable for CollectionView Added the selection feature for item in CollectionView

--- EdgeViewer/LibraryView/CollectionViewItem.swift
+++ EdgeViewer/LibraryView/CollectionViewItem.swift
@@ -9,12 +9,18 @@
 import Cocoa
 
 class CollectionViewItem: NSCollectionViewItem {
-    
-   
-    // 2
+    override var isSelected: Bool {
+        didSet {
+            view.layer?.borderWidth = isSelected ? 5.0 : 0.0
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.wantsLayer = true
         view.layer?.backgroundColor = NSColor.lightGray.cgColor
+        // Sets white boarder when the width is greater than zero
+        view.layer?.borderColor = NSColor.black.cgColor
+        // Sets the boarders to 0.0
+        view.layer?.borderWidth = 0.0
     }
 }

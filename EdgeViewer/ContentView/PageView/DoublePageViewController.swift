3b943453021e3bb97aef2c1c170bfcdef66b8902
YunSang Lee <qlqhwb@gmail.com>
2018-04-20 18:57:00 -0400

    Replace DoublePageView with DoublePageViewController

---
+++ EdgeViewer/ContentView/PageView/DoublePageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DoublePageViewController.swift
+//  EdgeViewer
+//
+//  Created by YunSang Lee on 2018. 4. 20..
+//  Copyright © 2018년 NoName. All rights reserved.
+//
+
+import Cocoa
+
+class DoublePageViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}

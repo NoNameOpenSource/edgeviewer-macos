ede40ce6b397d5d49916d8e71947ea196b4ed9a4
YunSang Lee <qlqhwb@gmail.com>
2018-04-20 03:19:59 -0400

    Cleaned unused code

--- EdgeViewer/ContentView/PageView/SinglePageViewController.swift
+++ EdgeViewer/ContentView/PageView/SinglePageViewController.swift
@@ -23,51 +23,6 @@ class SinglePageViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
-        //self.imageView!.image = self.image
-        //imageView.imageScaling = .scaleProportionallyDown
-    }
-    
-    override func viewDidAppear() {
-        // add constraints to fill entire view
-        //self.imageView.layer!.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-        //self.view.layer!.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
-        //self.view.superview!.layer!.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
-        //self.view.superview!.superview!.layer!.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-        
-        /*
-        NSLayoutConstraint(item: self.view,
-                           attribute: .bottom,
-                           relatedBy: .equal,
-                           toItem: self.view.superview,
-                           attribute: .bottom,
-                           multiplier: 1.0,
-                           constant: 0.0)
-            .isActive = true
-        NSLayoutConstraint(item: self.view,
-                           attribute: .top,
-                           relatedBy: .equal,
-                           toItem: self.view.superview,
-                           attribute: .top,
-                           multiplier: 1.0,
-                           constant: 0.0)
-            .isActive = true
-        NSLayoutConstraint(item: self.view,
-                           attribute: .leading,
-                           relatedBy: .equal,
-                           toItem: self.view.superview,
-                           attribute: .leading,
-                           multiplier: 1.0,
-                           constant: 0.0)
-            .isActive = true
-        NSLayoutConstraint(item: self.view,
-                           attribute: .trailing,
-                           relatedBy: .equal,
-                           toItem: self.view.superview,
-                           attribute: .trailing,
-                           multiplier: 1.0,
-                           constant: 0.0)
-            .isActive = true
- */
     }
     
 }

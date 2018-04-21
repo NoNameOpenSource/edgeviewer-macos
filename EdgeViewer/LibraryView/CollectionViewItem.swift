c7c05dd5c7e018e9e9b4892016c0cdcbc84c85d7
YunSang Lee <qlqhwb@gmail.com>
2018-04-21 15:54:01 -0400

    Change item color to green

--- EdgeViewer/LibraryView/CollectionViewItem.swift
+++ EdgeViewer/LibraryView/CollectionViewItem.swift
@@ -20,10 +20,10 @@ class CollectionViewItem: NSCollectionViewItem {
         
         // Do view setup here.
         view.wantsLayer = true
-        view.layer?.backgroundColor = NSColor.lightGray.cgColor
+        view.layer?.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
         
         // Sets white boarder when the width is greater than zero
-        view.layer?.borderColor = NSColor.black.cgColor
+        view.layer?.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         
         // Sets the boarders to 0.0
         view.layer?.borderWidth = 0.0

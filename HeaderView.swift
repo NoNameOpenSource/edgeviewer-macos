3eac182a04d269a02baf865418bd062236a062aa
YunSang Lee <qlqhwb@gmail.com>
2018-04-21 15:38:37 -0400

    Update the code with new API standard

--- HeaderView.swift
+++ HeaderView.swift
@@ -16,7 +16,7 @@ class HeaderView: NSView {
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
         NSColor(calibratedWhite: 0.8 , alpha: 0.8).set()
-        NSRectFillUsingOperation(dirtyRect, NSCompositingOperation.sourceOver)
+        dirtyRect.fill(using: NSCompositingOperation.sourceOver)
     }
     
 }

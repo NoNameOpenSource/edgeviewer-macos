0ae63185054bdfbf8fbe6296ae9c6dd42f941dc9
Matt Dean <mdean400@gmail.com>
2018-06-19 21:43:30 -0400

    Fix merge issue

--- EdgeViewer/LibraryView/LibraryViewController.swift
+++ EdgeViewer/LibraryView/LibraryViewController.swift
@@ -97,17 +97,6 @@ class LibraryViewController: NSSplitViewController, ShelfViewDelegate {
                                         self.removeSplitViewItem(self.splitViewItems[1])
                                         self.addSplitViewItem(NSSplitViewItem(viewController: contentViewController))
         })
-        oldShelf.prepare(for: segue, sender: user.self)
-        segue.perform()
-        let contentViewController = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil).instantiateController(withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "ContentViewController")) as! ContentViewController
-        let oldShelf = self.splitViewItems[1].viewController
-        let segue = NSStoryboardSegue(identifier: NSStoryboardSegue.Identifier(rawValue: "LibraryPageSegue"),
-                                      source: oldShelf,
-                                      destination: contentViewController,
-                                      performHandler: {
-                                        self.removeSplitViewItem(self.splitViewItems[1])
-                                        self.addSplitViewItem(NSSplitViewItem(viewController: contentViewController))
-        })
         contentViewController.book = book;
         oldShelf.prepare(for: segue, sender: user.self)
         segue.perform()

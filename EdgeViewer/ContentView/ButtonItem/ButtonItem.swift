6f4c2a9b20473672319b0c927904985dd70d8ea0
YunSang Lee <qlqhwb@gmail.com>
2018-06-26 10:37:47 -0400

    Change userPanel button style

--- EdgeViewer/ContentView/ButtonItem/ButtonItem.swift
+++ EdgeViewer/ContentView/ButtonItem/ButtonItem.swift
@@ -52,13 +52,13 @@ class ButtonItem: NSCollectionViewItem {
         
         // Do view setup here.
         view.wantsLayer = true
-        view.layer?.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        //view.layer?.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         
         // Sets white boarder when the width is greater than zero
-        view.layer?.borderColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        //view.layer?.borderColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
         
         // Sets the boarders to 0.0
-        view.layer?.borderWidth = 1.0
+        //view.layer?.borderWidth = 1.0
     }
     
 }

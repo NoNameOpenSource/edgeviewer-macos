121f763f23961a93804fcb972bdcfc789a3f7f8b
YunSang Lee <qlqhwb@gmail.com>
2018-10-07 23:21:26 +0900

    Fix bug: adding unrelated books to series

--- EdgeViewer/PluginAPI/LocalPlugin/LocalPlugin.swift
+++ EdgeViewer/PluginAPI/LocalPlugin/LocalPlugin.swift
@@ -111,13 +111,15 @@ class LocalPlugin: Plugin {
         do {
             let seriesFolders = try fileManager.contentsOfDirectory(at: booksDirectory!, includingPropertiesForKeys: nil, options: [])
             for (seriesFolder) in seriesFolders {
-                if !seriesFolder.absoluteString.hasSuffix(".DS_Store") {
+                if seriesFolder.lastPathComponent == series.title {
                     do {
                         let bookFolders = try fileManager.contentsOfDirectory(at: seriesFolder, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles])
                         for bookFolder in bookFolders {
-                            let bookIdentifier = (seriesFolder.lastPathComponent, bookFolder.lastPathComponent)
-                            if let book = book(withIdentifier: (seriesFolder.lastPathComponent, bookFolder.lastPathComponent)) {
-                                books.append(book)
+                            if(fileManager.fileExists(atPath: bookFolder.path + "/BookData.xml")){
+                                let bookIdentifier = (seriesFolder.lastPathComponent, bookFolder.lastPathComponent)
+                                if let book = book(withIdentifier: bookIdentifier) {
+                                    books.append(book)
+                                }
                             }
                         }
                     }

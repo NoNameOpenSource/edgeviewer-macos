8d902b5d59cac26b9b8332c95bb1bbe815811047
Zhichun Yao <yzc971210@gmail.com>
2018-11-04 10:30:15 -0500

    add missing file

---
+++ EdgeViewer/NotificationView/ImportNotification.swift
@@ -0,0 +1,19 @@
+//
+//  ImportNotification.swift
+//  EdgeViewer
+//
+//  Created by bmmkac on 11/3/18.
+//  Copyright © 2018 NoName. All rights reserved.
+//
+
+import Cocoa
+
+class ImportNotification: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}

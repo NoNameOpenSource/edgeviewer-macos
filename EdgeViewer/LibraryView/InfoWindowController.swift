cf03330983b942a4e318014740da661672dd6a77
YunSang Lee <qlqhwb@gmail.com>
2019-06-09 04:40:32 +0900

    Add InfoWindow

---
+++ EdgeViewer/LibraryView/InfoWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  InfoWindowController.swift
+//  EdgeViewer
+//
+//  Created by YunSang Lee on 2019. 6. 4..
+//  Copyright © 2019년 NoName. All rights reserved.
+//
+
+import Cocoa
+
+class InfoWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
